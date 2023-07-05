cb858c4bdc44d5909918ad2c392627671b823ab7
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-07-06 01:31:23 +0600

    [DONE] unknown Type

---
+++ module-1/src/unknown-type.ts
@@ -0,0 +1,15 @@
+const getMyCarSpeed = (speed: unknown) => {
+    if (typeof speed === 'number') {
+        return speed
+    }
+    else if (typeof speed === 'string')  {
+        return parseInt(speed)
+    }
+    else{
+        return 0;
+    }
+}
+
+getMyCarSpeed(100);
+getMyCarSpeed('100 kmh^-1');
+getMyCarSpeed(true);
