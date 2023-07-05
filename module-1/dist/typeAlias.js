874fe082b37d7c78baa8d9028c13e1712ae6e00d
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-07-06 01:01:40 +0600

    [DONE] type alias

--- module-1/dist/typeAlias.js
+++ module-1/dist/typeAlias.js
@@ -11,3 +11,10 @@ const crushObject2 = {
     profession: 'Housewife',
     address: 'Rangpur',
 };
+const calculate = (number1, number2, operation) => {
+    return operation(number1, number2);
+};
+console.log(calculate(10, 20, (x, y) => x + y));
+console.log(calculate(10, 20, (x, y) => x - y));
+console.log(calculate(10, 20, (x, y) => x * y));
+console.log(calculate(10, 20, (x, y) => x / y));
