0f3c7b70bd895e4915cc06130efa12feb1703426
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-07-06 01:44:50 +0600

    [DONE] ternary operator

---
+++ module-1/src/ternary-operator.ts
@@ -0,0 +1,10 @@
+const age: number = 22;
+
+if (age >= 18){
+    console.log('You are an adult');
+}
+else{
+    console.log('You are not an adult');
+}
+
+const isAdult: string = age >= 18 ? 'yes' : 'no';
