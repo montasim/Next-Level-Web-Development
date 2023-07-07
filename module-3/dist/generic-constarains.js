dc3b4fea4171f746bb7d4a8ca7e04afbceb5fcf3
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-07-07 11:00:47 +0600

    [DONE] generic constrains

--- module-3/dist/generic-constarains.js
+++ module-3/dist/generic-constarains.js
@@ -1,8 +1,14 @@
 "use strict";
+;
 const myInfo2 = {
     name: 'John Doe',
     age: 30,
     salary: 30000,
+};
+const myInfo3 = {
+    name: 'Rahim Mia',
+    age: 50,
+    salary: 50000,
 };
 // generic arrow function
 const addMeIntoMyCrushMind3 = (myInfo) => {
@@ -10,5 +16,7 @@ const addMeIntoMyCrushMind3 = (myInfo) => {
     const newData = Object.assign(Object.assign({}, myInfo), { crush });
     return newData;
 };
-const result7 = addMeIntoMyCrushMind(myInfo);
-result7.name;
+const result7 = addMeIntoMyCrushMind3(myInfo2);
+const result8 = addMeIntoMyCrushMind3(myInfo3);
+console.log(result7);
+console.log(result8);
--- module-3/src/generic-constarains.ts
+++ module-3/src/generic-constarains.ts
@@ -4,20 +4,34 @@ type MyInfoType = {
     salary: number
 };
 
+interface MyInfoInterface {
+    name: string,
+    age: number,
+    salary: number
+};
+
 const myInfo2: MyInfoType = {
     name: 'John Doe',
     age: 30,
     salary: 30000,
 };
 
+const myInfo3: MyInfoInterface = {
+    name: 'Rahim Mia',
+    age: 50,
+    salary: 50000,
+}
+
 // generic arrow function
-const addMeIntoMyCrushMind3 = <T> (myInfo: T) => {
+const addMeIntoMyCrushMind3 = <T extends MyInfoType> (myInfo: T) => {
     const crush = 'Kate Winslet';
     const newData = {...myInfo, crush};
 
     return newData;
 };
 
-const result7 = addMeIntoMyCrushMind(myInfo);
+const result7 = addMeIntoMyCrushMind3<MyInfoType>(myInfo2);
+const result8 = addMeIntoMyCrushMind3<MyInfoInterface>(myInfo3);
 
-result7.name;
+console.log(result7);
+console.log(result8);
